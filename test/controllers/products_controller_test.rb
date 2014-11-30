ea49507eae73b5e6bd1010fc42029c0731c92ad5
cartothemax <cartothemax@gmail.com>
2014-11-30 11:40:00 -0600

    basic product editing and listing

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,21 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_line_odd' do |elements|
+      elements.each do |element|
+        assert_select element, 'a', /Show/
+        assert_select element, 'a', /Edit/
+        assert_select element, 'a', /Destroy/
+      end
+    end
+    
+    assert_select '.list_line_even' do |elements|
+      elements.each do |element|
+        assert_select element, 'a', /Show/
+        assert_select element, 'a', /Edit/
+        assert_select element, 'a', /Destroy/
+      end
+    end
   end
 
   test "should get new" do
