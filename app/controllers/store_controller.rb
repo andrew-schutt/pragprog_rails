91b2ad50cfade9fdb4030a92f9e038c87ac5ff9e
cartothemax <cartothemax@gmail.com>
2014-12-18 21:40:51 -0600

    a bunch of changes...

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
     @products = Product.order(:title)
   end
 end
